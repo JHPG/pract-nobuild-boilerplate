4e354ad1b542707752116162be88c57423e88658
Jorge Garcia <jorge.garcia@gorilainvest.com.br>
2022-06-07 19:11:24 -0300

    docs(Readme): add Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Preact no-build Counter
+
+Access https://jhpg.github.io/preact-nobuild-boilerplate to check out the live demo.
+
+- Tip: Run with `npx live-server` locally;
--- deps.js
+++ deps.js
@@ -1,10 +1,5 @@
-import { h, Component, render } from 'https://unpkg.com/preact@10.7.3?module';
-
+import { html, Component, render } from 'https://unpkg.com/htm@3.1.1/preact/index.mjs?module'
 // import { useState, useEffect } from 'https://unpkg.com/preact@10.7.3/hooks/dist/hooks.module.js?module'; //FIXME: throws error using this way, use class components instead
-import htm from 'https://unpkg.com/htm@3.1.1?module';
-
-// Initialize htm with Preact
-const html = htm.bind(h);
 
 export {
     html, Component, render,
